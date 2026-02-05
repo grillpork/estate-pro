8381dda2e5a67e2254ad7d89631e1d27a8c4de6c
max <maxx.mg3@gmail.com>
2026-02-06 00:31:03 +0700

    update relational property

--- apps/backend/src/db/schemas/properties.ts
+++ apps/backend/src/db/schemas/properties.ts
@@ -1,7 +1,10 @@
-import { integer, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
+import { integer, json, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { user } from "./auth";
+import { relations } from "drizzle-orm";
 
-const propState = pgEnum("prop_state", ["pending", "approved", "rejected"]);
+
+
+export const propState = pgEnum("prop_state", ["pending", "approved", "rejected"]);
 
 export const properties = pgTable("properties", {
   id: text("id").primaryKey(),
@@ -11,9 +14,33 @@ export const properties = pgTable("properties", {
   price: integer("price").notNull(),
   address: text("address").notNull(),
   image: text("image"),
-  status: propState("status").notNull().default("pending"),
+  status: propState("status").default("pending").notNull(),
+
+// amenities: text("amenities").array(),
+  amenities: json("amenities").$type<string[]>().default([]),
+
   userId: text("user_id")
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
+
+     createdAt: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
 });
+
+ 
+export const propertyRelations = relations(properties, ({one}) => ({
+  //Many to one :อสังหา 1 หลัง ต่อ user 1 คน
+  Owner: one(user ,{
+    fields: [properties.userId],
+    references: [user.id]
+  })
+
+
+}))
+
+
 
